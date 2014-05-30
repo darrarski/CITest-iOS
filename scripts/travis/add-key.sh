d9b347190a42bcdd71c1661fb4a3d465c2ceed8b
Darrarski <darrarski@gmail.com>
2014-05-30 15:21:56 +0200

    add-key.sh travis script updated, modified paths

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -2,8 +2,8 @@ security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security -v set-keychain-settings -lut 86400 ios-build.keychain
-security import ./scripts/travis/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/travis/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/travis/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ../certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ../certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ../certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./scripts/travis/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ../profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
