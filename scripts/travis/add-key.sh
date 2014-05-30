0f4b7461426e38ff114683e92178e8cf93b7c113
Darrarski <darrarski@gmail.com>
2014-05-30 15:22:52 +0200

    add-key.sh travis script updated, debugging

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -2,6 +2,7 @@ security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security -v set-keychain-settings -lut 86400 ios-build.keychain
+pwd
 security import ../certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ../certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ../certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
